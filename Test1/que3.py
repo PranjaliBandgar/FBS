1c0f7b661408211986a6e6ecb83dd110a68c13a0
PranjaliBandgar <133084565+PranjaliBandgar@users.noreply.github.com>
2026-08-14 19:28:19 +0530

    Add files via upload

---
+++ Test1/que3.py
@@ -0,0 +1,7 @@
+dist = int(input('Enter the distance in km :'))
+
+meter = dist*1000
+centimeter = dist*1000*1000
+
+print(f'Distance is in meter is {meter}')
+print(f'Distance is in centimeter is {centimeter}')
